4678a4c075b8f055edcce1df2162885bdfd8eea9
nitsvutt <nitsvutt@gmail.com>
2025-10-26 14:27:10 +0700

    script to check node statistics

---
+++ scylladb/workspace/keyspace_statistics.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+nodetool cfstats -H sherlock | awk '
+BEGIN {
+  printf "%-30s %-30s %-20s %-20s %-20s\n", "Keyspace", "Table", "Live", "Total", "Memtable"
+}
+/^Keyspace : / {
+  ks = $3
+}
+/Table:/ {
+  t = $2
+}
+/Space used \(live\):/ {
+  live_size = $4 " " $5
+}
+/Space used \(total\):/ {
+  total_size = $4 " " $5
+}
+/Memtable data size:/ {
+  memtable_size = $4 " " $5
+  printf "%-30s %-30s %-20s %-20s %-20s\n", ks, t, live_size, total_size, memtable_size
+}
+'
